d59dfae334bff2f26934dbf6e80a816a28b1406b
Kharisma Fajar <restuistyo@gmail.com>
2023-02-26 01:33:05 +0700

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+// Change Bg Header
+function scrollHeader(){
+    const header = document.getElementById('header')
+    if(this.scrollY >= 50) header.classList.add('scroll-header');
+        else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+// Swiper Rumah
+
+// Accordion Keunggulan
+
+//Scroll Section Active Link
+
+// Show Scroll Up
+
+// Dark Light Theme
+
+// Scroll Reveal Anim
