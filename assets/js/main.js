c853eece08f2b29a8a0e3f1e5147637d5fa08b1d
Kharisma Fajar <restuistyo@gmail.com>
2023-02-27 15:47:15 +0700

    kontak

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,6 +21,31 @@ var swiperRumah = new Swiper(".rumah__container", {
   });
 
 // Accordion Keunggulan
+const accordionItems = document.querySelectorAll('.keunggulan__accordion-item')
+
+accordionItems.forEach((item) => {
+  const accordionheader = item.querySelector('.keunggulan__accordion-header')
+  accordionheader.addEventListener('click', () => {
+    const openItem = document.querySelector('.accordion-open')
+    toggleItem(item)
+
+    if(openItem && openItem!== item){
+      toggleItem(openItem)
+    }
+  })
+})
+
+const toggleItem = (item) =>{
+  const accordionContent = item.querySelector('.keunggulan__accordion-content')
+  if(item.classList.contains('accordion-open')){
+      accordionContent.removeAttribute('style')
+      item.classList.remove('accordion-open')
+  } else {
+      accordionContent.style.height = accordionContent.scrollHeight + 'px'
+      item.classList.add('accordion-open')
+  }
+  
+}
 
 //Scroll Section Active Link
 
